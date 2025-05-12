d0e92f8c0f994bda84e4eb68fcf8fd3bc8f86719
Zhekka <maratzhen@gmail.com>
2025-05-12 13:54:28 +0300

    refactor

--- js/site.js
+++ js/site.js
@@ -82,3 +82,34 @@ $(window).load(function () { // makes sure the whole site is loaded
 	}
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.gallery-swiper', {
+        loop: true,
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        spaceBetween: 30,
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            }
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
